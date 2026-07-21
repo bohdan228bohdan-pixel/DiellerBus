f9365e672b7815220906306c483010074ae24d54
bohdan228bohdan-pixel <bohdan228bohdan@gmail.com>
2026-07-21 21:35:02 +0300

    Add DATABASE_URL support for Render PostgreSQL

--- project/buswebsite/settings.py
+++ project/buswebsite/settings.py
@@ -156,12 +156,20 @@ WSGI_APPLICATION = 'buswebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
